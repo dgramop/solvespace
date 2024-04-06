360e8ae53b6f2332d0a23ea1ef0f4600db3b3183
Dhruv Gramopadhye <dgramopadhye@gmail.com>
2024-04-06 01:46:41 -0700

    stubs for relation constraint

--- src/mouse.cpp
+++ src/mouse.cpp
@@ -1161,6 +1161,19 @@ void GraphicsWindow::MouseLeftDown(double mx, double my, bool shiftDown, bool ct
                     hc = Constraint::AddConstraint(&c);
                     break;
                 }
+
+                case Command::RELATION: {
+                    //TODO
+                    ClearSuper();
+                    Constraint c = {};
+                    c.group       = SS.GW.activeGroup;
+                    c.workplane   = SS.GW.ActiveWorkplane();
+                    c.type        = Constraint::Type::RELATION;
+                    c.disp.offset = v;
+                    c.expression     = _("x");
+                    hc = Constraint::AddConstraint(&c);
+                    break;
+                }
                 default: ssassert(false, "Unexpected pending menu id");
             }
             break;
@@ -1491,6 +1504,7 @@ void GraphicsWindow::EditControlDone(const std::string &s) {
                 break;
             }
             case Constraint::Type::ANGLE:
+            case Constraint::Type::RELATION:
             case Constraint::Type::LENGTH_RATIO:
             case Constraint::Type::ARC_ARC_LEN_RATIO:
             case Constraint::Type::ARC_LINE_LEN_RATIO:
@@ -1510,7 +1524,7 @@ void GraphicsWindow::EditControlDone(const std::string &s) {
 
             default:
                 // These are always positive, and they get the units conversion.
-								// Use ExprToMm since this unparameterized expressions simplify down
+                // Use ExprToMm since this unparameterized expressions simplify down
                 c->valA = fabs(SS.ExprToMm(e));
                 break;
         }
