8686cce0f403b357ea266b86c8431c8a69768d1f
Dhruv Gramopadhye <dgramopadhye@gmail.com>
2026-09-12 12:10:36 -0700

    macos flake

--- default.nix
+++ default.nix
@@ -1,6 +1,10 @@
 { pkgs ? import <nixpkgs> {}, src ? ./. }:
 
-pkgs.stdenv.mkDerivation {
+let
+  inherit (pkgs) lib stdenv;
+  isDarwin = stdenv.isDarwin;
+in
+stdenv.mkDerivation {
   pname = "solvespace-themed";
   version = "3.2-themed";
 
@@ -9,16 +13,23 @@ pkgs.stdenv.mkDerivation {
   nativeBuildInputs = with pkgs; [
     cmake
     pkg-config
+  ] ++ lib.optionals (!isDarwin) [
     wrapGAppsHook3
   ];
 
   buildInputs = with pkgs; [
-    at-spi2-core
-    cairo
-    dbus
     eigen
     freetype
     fontconfig
+    libpng
+    opencascade-occt
+    zlib
+  ] ++ lib.optionals isDarwin [
+    llvmPackages.openmp
+  ] ++ lib.optionals (!isDarwin) [
+    at-spi2-core
+    cairo
+    dbus
     glew
     gtkmm3
     json_c
@@ -26,7 +37,6 @@ pkgs.stdenv.mkDerivation {
     libdeflate
     libepoxy
     libGLU
-    libpng
     libselinux
     libsepol
     libspnav
@@ -34,7 +44,6 @@ pkgs.stdenv.mkDerivation {
     libtiff
     libxkbcommon
     lerc
-    opencascade-occt
     pangomm
     pcre
     pcre2
@@ -43,37 +52,27 @@ pkgs.stdenv.mkDerivation {
     xorg.libXdmcp
     xorg.libXtst
     libsysprof-capture
-    zlib
   ];
-
-  prePatch = ''
-    # Initialize submodules if not present
-    mkdir -p extlib/libdxfrw extlib/mimalloc
-    if [ ! -f extlib/libdxfrw/CMakeLists.txt ]; then
-      cp -r ${pkgs.fetchFromGitHub {
-        owner = "solvespace";
-        repo = "libdxfrw";
-        rev = "8359399ff3eb96aec14fb160c9a5bc4796b60717";
-        hash = "sha256-tr8ISIxEj6nwMWaO4Vr9GtFFUaEEouAMev9fsNrmRqI=";
-      }}/* extlib/libdxfrw/
-      chmod -R u+w extlib/libdxfrw
-    fi
-    if [ ! -f extlib/mimalloc/CMakeLists.txt ]; then
-      cp -r ${pkgs.fetchFromGitHub {
-        owner = "microsoft";
-        repo = "mimalloc";
-        rev = "v2.2.2";
-        hash = "sha256-sunmnMlPAjlYLU7434CowDiEkgGdzcyYh7PC2vH4TrA=";
-      }}/* extlib/mimalloc/
-      chmod -R u+w extlib/mimalloc
-    fi
-  '';
 
   postPatch = ''
     # Patch out git hash requirement since we're building from local source
     substituteInPlace CMakeLists.txt \
       --replace "include(GetGitCommitHash)" "# include(GetGitCommitHash)" \
       --replace "# set(GIT_COMMIT_HASH 0000000000000000000000000000000000000000)" "set(GIT_COMMIT_HASH themed-fork)"
+  '' + lib.optionalString isDarwin ''
+    # OpenMP_CXX_INCLUDE_DIRS is empty when clang's -fopenmp=libomp discovers
+    # libomp implicitly, so LIBOMP_LIB_PATH resolves to /../lib/libomp.dylib.
+    # Point it at the actual dylib from llvmPackages.openmp.
+    substituteInPlace src/CMakeLists.txt \
+      --replace '${"\${OpenMP_CXX_INCLUDE_DIRS}"}/../lib/libomp.dylib' '${pkgs.llvmPackages.openmp}/lib/libomp.dylib'
+  '';
+
+  # macOS needs iconutil (icon bundle) and ibtool (nib compiler) from Xcode.
+  # /usr/bin/ibtool is a shim that dispatches through DEVELOPER_DIR, so we set
+  # both. Only reachable because Determinate Nix runs with sandbox disabled.
+  preBuild = lib.optionalString isDarwin ''
+    export PATH="$PATH:/usr/bin:/Applications/Xcode.app/Contents/Developer/usr/bin"
+    export DEVELOPER_DIR="/Applications/Xcode.app/Contents/Developer"
   '';
 
   cmakeFlags = [
@@ -81,10 +80,18 @@ pkgs.stdenv.mkDerivation {
     "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
   ];
 
-  meta = with pkgs.lib; {
+  # CMake has no install() rule for the macOS .app; copy it out of the build tree.
+  postInstall = lib.optionalString isDarwin ''
+    mkdir -p $out/Applications
+    cp -R bin/SolveSpace.app $out/Applications/
+    mkdir -p $out/bin
+    ln -s $out/Applications/SolveSpace.app/Contents/MacOS/SolveSpace $out/bin/SolveSpace
+  '';
+
+  meta = with lib; {
     description = "Parametric 3D CAD program with themeable text window";
     license = licenses.gpl3Plus;
-    platforms = platforms.linux;
+    platforms = platforms.linux ++ platforms.darwin;
     homepage = "https://solvespace.com";
   };
 }
--- flake.nix
+++ flake.nix
@@ -7,31 +7,47 @@
 
   outputs = { self, nixpkgs }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      solvespace-themed = pkgs.callPackage ./default.nix { src = self; };
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs supportedSystems
+          (system: f system nixpkgs.legacyPackages.${system});
     in {
-      packages.${system} = {
-        default = solvespace-themed;
-        solvespace-themed = solvespace-themed;
-      };
+      packages = forAllSystems (system: pkgs:
+        let
+          solvespace-themed = pkgs.callPackage ./default.nix { src = self; };
+        in {
+          default = solvespace-themed;
+          solvespace-themed = solvespace-themed;
+        });
 
-      devShells.${system}.default = pkgs.mkShell {
-        inputsFrom = [ solvespace-themed ];
+      devShells = forAllSystems (system: pkgs:
+        let
+          inherit (nixpkgs) lib;
+          solvespace-themed = self.packages.${system}.solvespace-themed;
+        in {
+          default = pkgs.mkShell ({
+            inputsFrom = [ solvespace-themed ];
 
-        packages = with pkgs; [
-          gdb
-          clang-tools
-          qt6.qtbase
-          qt6.wrapQtAppsHook
-          opencascade-occt
-          gsettings-desktop-schemas
-          dconf
-        ];
-
-        # GSettings environment for running unwrapped binaries during development
-        GIO_EXTRA_MODULES = "${pkgs.dconf.lib}/lib/gio/modules";
-        XDG_DATA_DIRS = "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}";
-      };
+            packages = with pkgs; [
+              clang-tools
+              qt6.qtbase
+              qt6.wrapQtAppsHook
+              opencascade-occt
+            ] ++ lib.optionals pkgs.stdenv.isLinux [
+              gdb
+              gsettings-desktop-schemas
+              dconf
+            ];
+          } // lib.optionalAttrs pkgs.stdenv.isLinux {
+            # GSettings environment for running unwrapped binaries during development
+            GIO_EXTRA_MODULES = "${pkgs.dconf.lib}/lib/gio/modules";
+            XDG_DATA_DIRS = "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}";
+          });
+        });
     };
 }
